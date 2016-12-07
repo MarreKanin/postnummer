746f04b76b274d6b09cfd0d529ba69672a83f0c9
Marius Andersen <marrekanin@gmail.com>
2016-12-07 21:13:50 +0100

    JS-updates

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# marre-webstart
-This is a basic start template for web development in pug and sass.
-asdas
+# postnummer
+Template for getting Norwegian postal names from postal codes.
+
+API-link: http://developer.bring.com/api/postal-code/
+
+Demo: https://marremaskin.github.io/postnummer/
+
+Source: http://codereview.stackexchange.com/questions/13212/encapsulation-of-client-side-logic-in-web-page
--- js/postnr.js
+++ js/postnr.js
@@ -1,58 +1,34 @@
-$(function () { // shorthand for $(document).ready(...)
+$(function () {
   var postNrCtrl   = $("#postnr"),
       postStedCtrl = $("#poststed");
 
   postNrCtrl.on("change", function () {
     var postnr = jQuery.trim(this.value);
 
-    // if the field is empty, clear the postStedCtrl and stop
     if( !postnr ) {
       postStedCtrl.val("");
       return;
     }
 
-    // Return if the value isn't 4 digits (i.e.
-    // if it isn't a valid Norwegian postal code)
+
     if( !/^\d{4}$/.test(postnr) ) {
-      // perhaps you want to alert the user here before returning
       return;
     }
 
-    // Make an explicit JSONP request to get around same-origin policy
-    // jQuery takes care of encoding the params and adding the callback param
+
     $.ajax({
       type:     "GET",
       dataType: "jsonp",
       url:      "https://fraktguide.bring.no/fraktguide/api/postalCode.json",
+      // https://fraktguide.bring.no/fraktguide/api/postalCode.json?pnr=7605
       data: {
-        country: "no",
+        country: "no",    // use "en" for english 
         pnr:     postnr
       }
     }).done(function (data) {
-      if( data && data.valid ) {       // check the response a little
-        postStedCtrl.val(data.result); // postStedCtrl is available via closure
-      }
+      // if( data && data.valid ) {     // if we dont want
+        postStedCtrl.val(data.result);
+      // }
     });
   });
 });
-
-
-
-
-// //Sjekke postnummeret /
-// $('.postcode').focusout(function () {
-//     var ref = $(this);
-//     if ($(this).val().length == 4) {
-//         //Vi bruker Postens postnummersjekk
-//         $.getJSON('https://fraktguide.bring.no/fraktguide/api/postalCode.json?pnr=' + $(this).val() + '&callback=?',
-//         function (data) {
-//             if (data.valid) {
-//                 ref.next('.post-location').val(data.result);
-//             } else {
-//                 ref.next('.post-location').val('Ugyldig postnummer');
-//             }
-//         });
-//     } else {
-//         $(this).next('.post-location').val('');
-//     }
-// });
